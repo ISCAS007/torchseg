b45dd8d05928f0d387331e930fb44cc0286f82db
yzbx <youdaoyzbx@163.com>
2018-07-31 10:49:47 +0800

    update train voc

--- models/mxnet/train_voc.sh
+++ models/mxnet/train_voc.sh
@@ -3,4 +3,6 @@
 #    'pascal_voc': VOCSegmentation,
 #    'pascal_aug': VOCAugSegmentation,
 #}
-python train.py --dataset pascal_voc --model psp --backbone resnet50 --lr 0.001 --checkname resnet50_psp_pascal
+# batch-size=4 ==> use-gpu-memory = 5311M
+# Epoch 49, validation pixAcc: 0.789, mIoU: 0.519: 100%|█| 91/91 [01:44<00:00,  1.15s/it]
+python train.py --dataset pascal_voc --model psp --backbone resnet50 --lr 0.001 --checkname resnet50_psp_pascal --batch-size 4
