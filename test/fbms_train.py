fb23300a299d31e22e7aea494f0fa7ffb44a31e7
yzbx <youdaoyzbx@163.com>
2019-02-26 13:54:17 +0800

    fix bugs

--- test/fbms_train.py
+++ test/fbms_train.py
@@ -16,26 +16,30 @@ from utils.disc_tools import str2bool
 
 class Metric_Acc():
     def __init__(self):
-        self.tp=0
-        self.fp=0
-        self.tn=0
-        self.fn=0
-        self.count=0
+        self.tp=0.0
+        self.fp=0.0
+        self.tn=0.0
+        self.fn=0.0
+        self.count=0.0
         
     def update(self,predicts,labels):
         # print(labels.shape,predicts.shape)
         if labels.shape != predicts.shape:
-            pred=torch.argmax(predicts,dim=1,keepdim=True)
+            pred=torch.argmax(predicts,dim=1,keepdim=True).type_as(labels)
         else:
-            pred=torch.ge(predicts,0.5).to(device).long()
-        self.tp+=torch.sum((pred*labels==1).to(torch.float))
-        self.fp+=torch.sum((pred-labels==1).to(torch.float))
-        self.tn+=torch.sum((pred+labels==0).to(torch.float))
-        self.fn+=torch.sum((labels-pred==1).to(torch.float))
+            #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            pred=(predicts>0.5).type_as(labels)
+        self.tp+=torch.sum((pred+labels==2))
+        self.fp+=torch.sum((pred-labels==1))
+        # 1+255==0 for torch.uint8
+        self.tn+=torch.sum((pred+pred+labels==0))
+        self.fn+=torch.sum((labels-pred==1))
             
-        self.count+=torch.sum((labels!=255).to(torch.float))
+        self.count+=torch.sum((labels<255))
         
-        assert self.tp+self.fp+self.tn+self.fn==self.count,'tp=%d, fp=%d, tn=%d, fn=%d, count=%d'%(self.tp,self.fp,self.tn,self.fn,self.count)
+        assert self.tp+self.fp+self.tn+self.fn==self.count, \
+        'tp={}; fp={}; tn={}; fn={}; count={} \n pred {}, labels {}'.format(self.tp,
+            self.fp,self.tn,self.fn,self.count,torch.unique(pred),torch.unique(labels))
         
     
     def get_acc(self):
@@ -140,137 +144,138 @@ def get_parser():
                         default='000')
     
     return parser
+
+if __name__ == '__main__':
+    parser=get_parser()
+    args = parser.parse_args()
     
-parser=get_parser()
-args = parser.parse_args()
-
-config={}
-config['dataset']=args.dataset
-config['use_part_number']=args.use_part_number
-config['ignore_outOfRoi']=args.ignore_outOfRoi
-config['net_name']=args.net_name
-
-if args.dataset=='FBMS':
-    config['train_path']='dataset/FBMS/Trainingset'
-    config['test_path']=config['val_path']='dataset/FBMS/Testset'
-elif args.dataset=='cdnet2014':
-    config['root_path']='dataset/cdnet2014'
-else:
-    assert False
+    config={}
+    config['dataset']=args.dataset
+    config['use_part_number']=args.use_part_number
+    config['ignore_outOfRoi']=args.ignore_outOfRoi
+    config['net_name']=args.net_name
     
-config['frame_gap']=5
-config['log_dir']=os.path.expanduser('~/tmp/logs/motion')
-config['epoch']=30
-config['init_lr']=1e-4
-config['stn_loss_weight']=args.stn_loss_weight
-config['motion_loss_weight']=args.motion_loss_weight
-config['pose_mask_reg']=args.pose_mask_reg
-# features or images
-config['stn_object']=args.stn_object
-config['note']=args.note
-config['save_model']=args.save_model
-config['backbone_name']=args.backbone_name
-config['upsample_layer']=args.upsample_layer
-config['freeze_layer']=args.freeze_layer
-
-normer=image_normalizations(ways='-1,1')
-dataset_loaders={}
-for split in ['train','val']:
     if args.dataset=='FBMS':
-        xxx_dataset=fbms_dataset(config,split,normalizations=normer)
+        config['train_path']='dataset/FBMS/Trainingset'
+        config['test_path']=config['val_path']='dataset/FBMS/Testset'
+    elif args.dataset=='cdnet2014':
+        config['root_path']='dataset/cdnet2014'
     else:
-        xxx_dataset=cdnet_dataset(config,split,normalizations=normer)
-    
-    xxx_loader=td.DataLoader(dataset=xxx_dataset,batch_size=4,shuffle=True,drop_last=False,num_workers=2)
-    dataset_loaders[split]=xxx_loader
-
-time_str = time.strftime("%Y-%m-%d___%H-%M-%S", time.localtime())
-log_dir = os.path.join(config['log_dir'], config['net_name'],
-                       config['dataset'], config['note'], time_str)
-checkpoint_path = os.path.join(log_dir, 'model-last-%d.pkl' % config['epoch'])
-
-writer=init_writer(config,log_dir)
-
-if config['net_name'] in ['motion_stn','motion_net']:
-    model=globals()[config['net_name']]() 
-else:
-    model=globals()[config['net_name']](config)
-
-seg_loss_fn=torch.nn.CrossEntropyLoss(ignore_index=255)
-
-# support for cpu/gpu
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
-
-optimizer_params = [{'params': [p for p in model.parameters() if p.requires_grad]}]
-optimizer = torch.optim.Adam(
-            optimizer_params, lr=config['init_lr'], amsgrad=False)
-
-metric_acc=Metric_Acc()
-metric_stn_loss=Metric_Mean()
-metric_mask_loss=Metric_Mean()
-metric_total_loss=Metric_Mean()
-
-for epoch in range(config['epoch']):
-    for split in ['train','val']:
-        if split=='train':
-            model.train()
-        else:
-            model.eval()
-            
-        metric_acc.reset()
-        metric_stn_loss.reset()
-        metric_mask_loss.reset()
-        metric_total_loss.reset()
-        for frames,gt in dataset_loaders[split]:
-            images = [torch.autograd.Variable(img.to(device).float()) for img in frames]
-            labels=torch.autograd.Variable(gt.to(device).long())
-            
-            if split=='train':
-                optimizer.zero_grad()
-                
-            outputs=model.forward(images)
-            mask_loss_value=seg_loss_fn(outputs['masks'][0],torch.squeeze(labels,dim=1))
-            
-            # config['net_name'].find('stn')>=0
-            if config['net_name'] in ['motion_stn','motion_fcn_stn','motion_unet_stn']:
-                if config['stn_object']=='features':
-                    stn_loss_value=stn_loss(outputs['features'],labels.float(),outputs['pose'],config['pose_mask_reg'])
-                elif config['stn_object']=='images':
-                    stn_loss_value=stn_loss(outputs['stn_images'],labels.float(),outputs['pose'],config['pose_mask_reg'])
-                else:
-                    assert False,'unknown stn object %s'%config['stn_object']
-                
-                total_loss_value=mask_loss_value*config['motion_loss_weight']+stn_loss_value*config['stn_loss_weight']
-            else:
-                stn_loss_value=torch.tensor(0.0)
-                total_loss_value=mask_loss_value
-            metric_acc.update(outputs['masks'][0],labels)
-            metric_stn_loss.update(stn_loss_value.item())
-            metric_mask_loss.update(mask_loss_value.item())
-            metric_total_loss.update(total_loss_value.item())
-            if split=='train':
-                total_loss_value.backward()
-                optimizer.step()
-        acc=metric_acc.get_acc()
-        precision=metric_acc.get_precision()
-        recall=metric_acc.get_recall()
-        fmeasure=metric_acc.get_fmeasure()
-        mean_stn_loss=metric_stn_loss.get_mean()
-        mean_mask_loss=metric_mask_loss.get_mean()
-        mean_total_loss=metric_total_loss.get_mean()
-        writer.add_scalar(split+'/acc',acc,epoch)
-        writer.add_scalar(split+'/precision',precision,epoch)
-        writer.add_scalar(split+'/recall',recall,epoch)
-        writer.add_scalar(split+'/fmeasure',fmeasure,epoch)
-        writer.add_scalar(split+'/stn_loss',mean_stn_loss,epoch)
-        writer.add_scalar(split+'/mask_loss',mean_mask_loss,epoch)
-        writer.add_scalar(split+'/total_loss',mean_total_loss,epoch)
+        assert False
         
-        if epoch % 10 == 0:
-            print(split,'acc=%0.4f'%acc,'loss=',mean_total_loss)
-
-if config['save_model']:
-    torch.save(model.state_dict(),checkpoint_path)
-
-writer.close()
+    config['frame_gap']=5
+    config['log_dir']=os.path.expanduser('~/tmp/logs/motion')
+    config['epoch']=30
+    config['init_lr']=1e-4
+    config['stn_loss_weight']=args.stn_loss_weight
+    config['motion_loss_weight']=args.motion_loss_weight
+    config['pose_mask_reg']=args.pose_mask_reg
+    # features or images
+    config['stn_object']=args.stn_object
+    config['note']=args.note
+    config['save_model']=args.save_model
+    config['backbone_name']=args.backbone_name
+    config['upsample_layer']=args.upsample_layer
+    config['freeze_layer']=args.freeze_layer
+    
+    normer=image_normalizations(ways='-1,1')
+    dataset_loaders={}
+    for split in ['train','val']:
+        if args.dataset=='FBMS':
+            xxx_dataset=fbms_dataset(config,split,normalizations=normer)
+        else:
+            xxx_dataset=cdnet_dataset(config,split,normalizations=normer)
+        
+        xxx_loader=td.DataLoader(dataset=xxx_dataset,batch_size=4,shuffle=True,drop_last=False,num_workers=2)
+        dataset_loaders[split]=xxx_loader
+    
+    time_str = time.strftime("%Y-%m-%d___%H-%M-%S", time.localtime())
+    log_dir = os.path.join(config['log_dir'], config['net_name'],
+                           config['dataset'], config['note'], time_str)
+    checkpoint_path = os.path.join(log_dir, 'model-last-%d.pkl' % config['epoch'])
+    
+    writer=init_writer(config,log_dir)
+    
+    if config['net_name'] in ['motion_stn','motion_net']:
+        model=globals()[config['net_name']]() 
+    else:
+        model=globals()[config['net_name']](config)
+    
+    seg_loss_fn=torch.nn.CrossEntropyLoss(ignore_index=255)
+    
+    # support for cpu/gpu
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    
+    optimizer_params = [{'params': [p for p in model.parameters() if p.requires_grad]}]
+    optimizer = torch.optim.Adam(
+                optimizer_params, lr=config['init_lr'], amsgrad=False)
+    
+    metric_acc=Metric_Acc()
+    metric_stn_loss=Metric_Mean()
+    metric_mask_loss=Metric_Mean()
+    metric_total_loss=Metric_Mean()
+    
+    for epoch in range(config['epoch']):
+        for split in ['train','val']:
+            if split=='train':
+                model.train()
+            else:
+                model.eval()
+                
+            metric_acc.reset()
+            metric_stn_loss.reset()
+            metric_mask_loss.reset()
+            metric_total_loss.reset()
+            for frames,gt in dataset_loaders[split]:
+                images = [torch.autograd.Variable(img.to(device).float()) for img in frames]
+                labels=torch.autograd.Variable(gt.to(device).long())
+                
+                if split=='train':
+                    optimizer.zero_grad()
+                    
+                outputs=model.forward(images)
+                mask_loss_value=seg_loss_fn(outputs['masks'][0],torch.squeeze(labels,dim=1))
+                
+                # config['net_name'].find('stn')>=0
+                if config['net_name'] in ['motion_stn','motion_fcn_stn','motion_unet_stn']:
+                    if config['stn_object']=='features':
+                        stn_loss_value=stn_loss(outputs['features'],labels.float(),outputs['pose'],config['pose_mask_reg'])
+                    elif config['stn_object']=='images':
+                        stn_loss_value=stn_loss(outputs['stn_images'],labels.float(),outputs['pose'],config['pose_mask_reg'])
+                    else:
+                        assert False,'unknown stn object %s'%config['stn_object']
+                    
+                    total_loss_value=mask_loss_value*config['motion_loss_weight']+stn_loss_value*config['stn_loss_weight']
+                else:
+                    stn_loss_value=torch.tensor(0.0)
+                    total_loss_value=mask_loss_value
+                metric_acc.update(outputs['masks'][0],labels)
+                metric_stn_loss.update(stn_loss_value.item())
+                metric_mask_loss.update(mask_loss_value.item())
+                metric_total_loss.update(total_loss_value.item())
+                if split=='train':
+                    total_loss_value.backward()
+                    optimizer.step()
+            acc=metric_acc.get_acc()
+            precision=metric_acc.get_precision()
+            recall=metric_acc.get_recall()
+            fmeasure=metric_acc.get_fmeasure()
+            mean_stn_loss=metric_stn_loss.get_mean()
+            mean_mask_loss=metric_mask_loss.get_mean()
+            mean_total_loss=metric_total_loss.get_mean()
+            writer.add_scalar(split+'/acc',acc,epoch)
+            writer.add_scalar(split+'/precision',precision,epoch)
+            writer.add_scalar(split+'/recall',recall,epoch)
+            writer.add_scalar(split+'/fmeasure',fmeasure,epoch)
+            writer.add_scalar(split+'/stn_loss',mean_stn_loss,epoch)
+            writer.add_scalar(split+'/mask_loss',mean_mask_loss,epoch)
+            writer.add_scalar(split+'/total_loss',mean_total_loss,epoch)
+            
+            if epoch % 10 == 0:
+                print(split,'fmeasure=%0.4f'%fmeasure,'loss=',mean_total_loss)
+    
+    if config['save_model']:
+        torch.save(model.state_dict(),checkpoint_path)
+    
+    writer.close()
