bc1a0fd87178944925c74466571d65e023a66c69
yzbx <youdaoyzbx@163.com>
2019-08-28 10:56:02 +0800

    add unittest

---
+++ unittests/motionseg_test.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from easydict import EasyDict as edict
+import os
+from dataset.fbms_dataset import fbms_dataset
+from dataset.cdnet_dataset import cdnet_dataset
+from dataset.segtrackv2_dataset import segtrackv2_dataset
+from dataset.bmcnet_dataset import bmcnet_dataset
+import unittest
+import cv2
+
+def get_dataset(config,split,normer=None,augmentations=None):
+    if config.dataset=='FBMS':
+        config['root_path']=os.path.expanduser('~/cvdataset/FBMS')
+    elif config.dataset=='cdnet2014':
+        config['root_path']=os.path.expanduser('~/cvdataset/cdnet2014')
+    elif config.dataset=='segtrackv2':
+        config['root_path']=os.path.expanduser('~/cvdataset/SegTrackv2')
+    elif config.dataset=='BMCnet':
+        config['root_path']=os.path.expanduser('~/cvdataset/BMCnet')
+    else:
+        assert False
+
+    if config.dataset=='FBMS':
+        xxx_dataset=fbms_dataset(config,split,normalizations=normer,augmentations=augmentations)
+    elif config.dataset=='cdnet2014':
+        xxx_dataset=cdnet_dataset(config,split,normalizations=normer,augmentations=augmentations)
+        print(xxx_dataset.train_set,xxx_dataset.val_set)
+    elif config.dataset=='segtrackv2':
+        xxx_dataset=segtrackv2_dataset(config,split,normalizations=normer,augmentations=augmentations)
+    elif config.dataset=='BMCnet':
+        xxx_dataset=bmcnet_dataset(config,split,normalizations=normer,augmentations=augmentations)
+    else:
+        assert False
+
+    return xxx_dataset
+
+class Test(unittest.TestCase):
+    config=edict()
+    config.dataset='FBMS'
+    config.frame_gap=0
+    config.input_shape=(224,224)
+    config.use_part_number=1000
+    config.use_optical_flow=False
+    config.ignore_outOfRoi=True
+    def test_dataset(self):
+        def test_img(p):
+            try:
+                img=cv2.imread(p)
+            except Exception as e:
+                print(dataset,p,e)
+            else:
+                self.assertTrue(min(img.shape)>0,p)
+
+        for dataset in ['FBMS','cdnet2014','segtrackv2','BMCnet']:
+            self.config.dataset=dataset
+            xxx_dataset=get_dataset(self.config,'train')
+            N=len(xxx_dataset)
+            for i in range(N):
+                main,aux,gt=xxx_dataset.__get_path__(i)
+                for p in [main,aux,gt]:
+                    if isinstance(p,str):
+                        test_img(p)
+                    else:
+                        for x in p:
+                            test_img(x)
+
+
+if __name__ == '__main__':
+    unittest.main()
