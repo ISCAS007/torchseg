76bde41768cc4099eda19044eda70b58f01de201
yzbx <youdaoyzbx@163.com>
2019-08-29 18:38:06 +0800

    haha

--- dataset/segtrackv2_dataset.py
+++ dataset/segtrackv2_dataset.py
@@ -26,17 +26,84 @@ def main2flow(main_path,
 
     return out_path
 
-class segtrackv2_dataset(td.Dataset):
+class motionseg_dataset(td.Dataset):
     """
-    multi instances tracking dataset
+    base class dataset
     """
     def __init__(self,config,split='train',normalizations=None,augmentations=None):
         self.config=config
         self.split=split
         self.normalizations=normalizations
         self.augmentations=augmentations
+
+    def __getimage__(self,index):
+        """
+        return frame_images,gt_image,main_path,aux_path,gt_path
+        """
+        assert False
+
+    def __getitem__(self,index):
+        frame_images,gt_image,main_path,aux_path,gt_path=self.__getimage__(index)
+
+        # augmentation dataset
+        if self.split=='train' and self.augmentations is not None:
+            frame_images=[self.augmentations.transform(img) for img in frame_images]
+
+        # resize image
+        resize_frame_images=[cv2.resize(img,self.input_shape,interpolation=cv2.INTER_LINEAR) for img in frame_images]
+        if self.split=='train':
+            resize_gt_image=cv2.resize(gt_image,self.input_shape,interpolation=cv2.INTER_NEAREST)
+        else:
+            resize_gt_image=gt_image
+
+        # normalize image
+        if self.normalizations is not None:
+            resize_frame_images = [self.normalizations.forward(img) for img in resize_frame_images]
+
+        # bchw
+        resize_frame_images=[img.transpose((2,0,1)) for img in resize_frame_images]
+        resize_gt_image=np.expand_dims(resize_gt_image,0)
+
+        resize_gt_image=(resize_gt_image!=0).astype(np.uint8)
+
+        if self.use_optical_flow:
+            flow_path=main2flow(main_path)
+            flow_file=open(flow_path,'r')
+            a=np.fromfile(flow_file,np.uint8,count=4)
+            b=np.fromfile(flow_file,np.int32,count=2)
+            flow=np.fromfile(flow_file,np.float32).reshape((b[1],b[0],2))
+            flow=np.clip(flow,a_min=-50,a_max=50)/50.0
+            optical_flow=cv2.resize(flow,self.input_shape,interpolation=cv2.INTER_LINEAR).transpose((2,0,1))
+            if self.split=='val_path':
+                return {'images':[resize_frame_images[0],optical_flow],
+                        'gt':resize_gt_image,
+                        'gt_path':gt_path,
+                        'shape':frame_images[0].shape}
+            else:
+                return [resize_frame_images[0],optical_flow],resize_gt_image
+        else:
+            if self.split=='val_path':
+                return {'images':resize_frame_images,
+                        'gt':resize_gt_image,
+                        'gt_path':gt_path,
+                        'shape':frame_images[0].shape}
+            else:
+                return resize_frame_images,resize_gt_image
+
+class segtrackv2_dataset(motionseg_dataset):
+    """
+    multi instances tracking dataset
+    """
+    def __init__(self,config,split='train',normalizations=None,augmentations=None):
+        super().__init__(config,split,normalizations,augmentations)
+        self.config=config
+        self.split=split
+        self.normalizations=normalizations
+        self.augmentations=augmentations
         self.input_shape=tuple(config.input_shape)
         self.root_path=config.root_path
+        self.frame_gap=config.frame_gap
+        self.use_optical_flow=config.use_optical_flow
         self.main_files=self.get_main_files()
 
         print('dataset size = {}',len(self.main_files))
@@ -45,9 +112,6 @@ class segtrackv2_dataset(td.Dataset):
             gap=n//self.config['use_part_number']
             self.main_files=self.main_files[::gap]
             print('total dataset image %d, use %d'%(n,len(self.main_files)))
-
-        self.frame_gap=config.frame_gap
-        self.use_optical_flow=config.use_optical_flow
 
     def __len__(self):
         return len(self.main_files)
